08ef8cda476f5ee0eede3cf2a7132c33e3c044e3
shubham-saboo-789 <127737670+shubham-saboo-789@users.noreply.github.com>
2024-07-07 23:28:02 +0530

    Update sms.py
    removed ids

--- sms.py
+++ sms.py
@@ -5,14 +5,14 @@ def SMSing():
 	number = input("Enter number with the country code: ")
 	message = input("Enter the message you want to send: ")
     
-	account_sid = 'AC1fd7f4a2b23509026c9708d74bf82465'
-	auth_token = 'fbfdf69ce5ab33e329ad1fcd7a3e7934'
+	account_sid = 'AC1fd7f4a*******026c9708d******465'
+	auth_token = 'f***f69ce********9ad1fcd7****934'
 
 	client = Client(account_sid, auth_token)
 
 	message = client.messages.create(
 	    body=message,
-	    from_='+15076292590',
+	    from_='+150******90',
 	    to=number
 	)
 
@@ -20,14 +20,14 @@ def SMSing():
 
 def SMSing_sch(number,message):
     
-	account_sid = 'AC1fd7f4a2b23509026c9708d74bf82465'
-	auth_token = 'fbfdf69ce5ab33e329ad1fcd7a3e7934'
+	account_sid = 'A***d7f4a2b********c9708d7******65'
+	auth_token = 'fbf***9c****33e329a********e7934'
 
 	client = Client(account_sid, auth_token)
 
 	message = client.messages.create(
 	    body=message,
-	    from_='+15076292590',
+	    from_='+15******590',
 	    to=number
 	)
 
